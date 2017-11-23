7e77a5a10b1a50b58e49d91d2f0421a61d5596d0
tifa64 <krietallo@gmail.com>
2017-11-23 22:34:44 +0200

    Read 2 matrices

--- main.c
+++ main.c
@@ -22,23 +22,44 @@ void buildMat(int mat[][1000], int i, int j, char *line, int *columns) {
 }
 int main()
 {
-    FILE * fp;
-    char * line = NULL;
-    int mat[1000][1000];
+    FILE * fp1, * fp2;
+    char * line1 = NULL, * line2 = NULL;
+    int mat1[1000][1000], mat2[1000][1000];
     int i = 0, j = 0;
-    int rows = 0, *columns = 0;
+    int rows1 = 0, *columns1 = 0, rows2 = 0, *columns2 = 0;
     size_t len = 0;
-    ssize_t read;
-    fp = fopen("Matrix.txt", "r");
-    if (fp == NULL)
+    ssize_t read1, read2;
+    fp1 = fopen("Matrix1.txt", "r");
+    if (fp1 == NULL)
         exit(EXIT_FAILURE);
-    while ((read = getline(&line, &len, fp)) != -1) {
-        rows ++;
-        buildMat(mat, i++, read, line, &columns);
+    while ((read1 = getline(&line1, &len, fp1)) != -1) {
+        rows1 ++;
+        buildMat(mat1, i++, read1, line1, &columns1);
     }
-    fclose(fp);
-    if (line)
-        free(line);
+    fclose(fp1);
+    if (line1)
+        free(line1);
+
+    i = j = len = 0;
+    fp2 = fopen("Matrix2.txt", "r");
+    if (fp2 == NULL)
+        exit(EXIT_FAILURE);
+    while ((read2 = getline(&line2, &len, fp2)) != -1) {
+        rows2 ++;
+        buildMat(mat2, i++, read2, line2, &columns2);
+    }
+    fclose(fp2);
+    if (line2)
+        free(line2);
+
+    intk;
+    for (i = 0 ; i < rows1 ; i++){
+        for(j = 0 ; j < columns2 ; j++){
+            for()
+        }
+    }
+
+
     exit(EXIT_SUCCESS);
     return 0;
 }
